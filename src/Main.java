0ce8fae721354e9b4bb36ff0c86d7249e8b76354
josemanuelborja <jmborja11296@liceo.edu.ph>
2025-08-18 10:03:51 +0800

    lab-actiivty-4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Main Branch!");
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        String studentName = "Jose Manuel";
+        double average = student.calculateAverage(85.5, 92.0,78.5, 90.0);
+        String letterGrade = student.getLetterGrade(average);
+
+        student.displayResult(studentName, average);
+        student.displayResult(studentName, average, letterGrade);
     }
 }
