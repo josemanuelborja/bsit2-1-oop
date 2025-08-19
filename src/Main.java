929a6d64526e603bfdf3c2cbcb0cfd875888ed69
josemanuelborja <jmborja11296@liceo.edu.ph>
2025-08-19 11:56:31 +0800

    lab-activity-5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Main Branch!");
+    public static void main(String[] args) {
+        System.out.println("Bank Name: " + BankAccount.bankName);
+        System.out.println("Interest Rate: " + (BankAccount.interestRate * 100) + "%\n");
+
+        BankAccount acc1 = new BankAccount("Cabalde", 1000);
+        BankAccount acc2 = new BankAccount("Johnden", 2500);
+        BankAccount acc3 = new BankAccount("Ashton", 500);
+
+        System.out.println("\n═══ Account Operations ═══");
+        acc1.deposit(500);
+        acc2.withdraw(300);
+
+        System.out.println("\n═══ Interest Calculation ═══");
+        System.out.println("Cabalde's interest: $" + acc1.calculateInterest());
+        System.out.println("Johnden's interest: $" + acc2.calculateInterest());
+        System.out.println("Ashton's interest: $" + acc3.calculateInterest());
+
+        System.out.println("\nTotal Accounts Created: " + BankAccount.totalAccounts);
     }
 }
